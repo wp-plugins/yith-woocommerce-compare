a43643c50ab614062dd42194bff6304dfebb9dfb
yithemes <yithemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-05 14:19:21 +0000

    Version 1.1.3
    git-svn-id: https://plugins.svn.wordpress.org/yith-woocommerce-compare/tags/1.1.3@927162 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.yith-woocompare.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Functions
+ *
+ * @author Your Inspiration Themes
+ * @package YITH Woocommerce Compare
+ * @version 1.1.3
+ */
+
+if ( !defined( 'YITH_WOOCOMPARE' ) ) { exit; } // Exit if accessed directly
+
